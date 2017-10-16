c2555dbd101fafa42aa43bd2df6d6b5f2609dc16
Darek Kaczyński <kaczynskid@gmail.com>
2017-10-16 15:51:26 +0200

    Added reservation persistence

--- reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -2,10 +2,14 @@ package com.example.reservationservice;
 
 import static java.util.function.Function.*;
 import static java.util.stream.Collectors.*;
+import static javax.persistence.GenerationType.*;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.ResponseEntity.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
@@ -17,8 +21,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,17 +39,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @SpringBootApplication
+@EnableJpaRepositories
 public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServiceApplication.class, args);
 	}
+
+	@Autowired
+	ReservationRepository reservations;
+
+	@Bean
+	public ApplicationRunner init() {
+		return args -> {
+			long count = Arrays
+					.stream("Konrad,Mariusz,Adam,Michal,Lukasz,Przemek,Adam,Kamil,Marcin,Maciek".split(","))
+					.map(Reservation::new)
+					.map(reservations::save)
+					.collect(toList())
+					.size();
+			log.info("Added {} reservations.", count);
+		};
+	}
+}
+
+@RepositoryRestResource
+interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
 }
 
 @Slf4j
 @RestController
-@RequestMapping("/reservations")
+@RequestMapping("/myreservations")
 class ReservationController {
 
 	private Map<String, Reservation> reservations = Arrays
@@ -93,11 +126,20 @@ class AlreadyReserved extends RuntimeException {
 }
 
 @Data
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 class Reservation {
 
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	private Long id;
+
 	private String name;
+
+	Reservation(String name) {
+		this.name = name;
+	}
 }
 
 @Value
