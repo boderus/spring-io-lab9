e3776c51e8b2be63f079b84a5d53ece4c5b0cac6
Darek Kaczyński <kaczynskid@gmail.com>
2017-10-16 13:50:40 +0200

    Added some tests

---
+++ reservation-service/src/test/java/com/example/reservationservice/ReservationControllerTest.java
@@ -0,0 +1,42 @@
+package com.example.reservationservice;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ReservationController.class)
+public class ReservationControllerTest {
+
+	@Autowired
+	MockMvc mvc;
+
+	@Test
+	public void shouldFindExistingReservation() throws Exception {
+		mvc.perform(get("/reservations/{name}", "Adam"))
+				.andDo(print())
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void shouldNotFindNotExistingReservation() throws Exception {
+		mvc.perform(get("/reservations/{name}", "test"))
+			.andDo(print())
+			.andExpect(status().isNotFound());
+	}
+
+	public void shouldCreateReservation() throws Exception {
+
+	}
+
+	public void shouldNotCreateDuplicatedReservation() throws Exception {
+
+	}
+}
