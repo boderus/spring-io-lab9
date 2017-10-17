5dc21205233f7a01034b176d9b402627cc94bdd4
Darek Kaczyński <kaczynskid@gmail.com>
2017-10-17 16:35:29 +0200

    Added gateway

--- reservation-client/src/main/java/com/example/reservationclient/ReservationClientApplication.java
+++ reservation-client/src/main/java/com/example/reservationclient/ReservationClientApplication.java
@@ -1,6 +1,6 @@
 package com.example.reservationclient;
 
-import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -12,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -24,7 +23,6 @@ import org.springframework.hateoas.Resources;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -75,7 +73,6 @@ interface ReservationsClient {
 
 @Slf4j
 @RestController
-@RequestMapping("/client")
 class ReservationsController {
 
 	private final RestTemplate rest;
