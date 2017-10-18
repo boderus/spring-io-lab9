c96ba9c3187caa6b4f69fb1cc5baa20f0cb1ed61
Darek Kaczyński <kaczynskid@gmail.com>
2017-10-18 15:18:03 +0200

    Added integration with tracing

--- verifier-service/src/main/java/com/example/verifierservice/VerifierServiceApplication.java
+++ verifier-service/src/main/java/com/example/verifierservice/VerifierServiceApplication.java
@@ -10,11 +10,13 @@ import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VerifierServiceApplication {
 
 	public static void main(String[] args) {
@@ -27,8 +29,9 @@ public class VerifierServiceApplication {
 class VerifierController {
 
 	@RequestMapping(path = "/check", method = POST, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-	public VerificationResult check(@RequestBody VerificationRequest request) {
+	public VerificationResult check(@RequestBody VerificationRequest request) throws InterruptedException {
 		log.info("GOT REQUEST: {}", request);
+		Thread.sleep(500);
 		if (request.getAge() >= 20) {
 			return new VerificationResult(true);
 		} else {
