80199604393acdb7ea7bda92daf40cd4f41ebc13
Darek Kaczyński <kaczynskid@gmail.com>
2017-10-18 13:57:01 +0200

    Preconfigured verifier tests base

---
+++ verifier-service/src/test/java/com/example/verifierservice/VerifierServiceApplicationTestsBase.java
@@ -0,0 +1,27 @@
+package com.example.verifierservice;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = VerifierServiceApplication.class)
+public class VerifierServiceApplicationTestsBase {
+
+	@Autowired
+	WebApplicationContext applicationContext;
+
+	@Before
+	public void setup() {
+		RestAssuredMockMvc.webAppContextSetup(applicationContext);
+	}
+
+}
