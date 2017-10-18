d8e59f1837cd72dee05c766fe332060420afb489
Darek Kaczyński <kaczynskid@gmail.com>
2017-10-18 11:35:26 +0200

    Added verifier service

---
+++ verifier-service/src/main/java/com/example/verifierservice/VerifierServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.verifierservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifierServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerifierServiceApplication.class, args);
+	}
+}
---
+++ verifier-service/src/test/resources/contracts/shouldNotAllowReservationIfTooYoung.groovy
@@ -0,0 +1,7 @@
+package contracts
+
+import org.springframework.cloud.contract.spec.Contract
+
+Contract.make {
+
+}
